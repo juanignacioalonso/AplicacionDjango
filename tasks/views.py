57e1cc21a45305cdeab256536d5682187cc308b3
juanignacioalonso <zalbak87@gmail.com>
2025-09-19 15:58:28 -0300

    login

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.contrib.auth import login
+from django.db import IntegrityError
 
 
 # Create your views here.
@@ -20,9 +21,10 @@ def singup(request):
                     try:
                         user = User.objects.create_user(username=request.POST['username'] , password=request.POST['password1'])
                         user.save()
-                        return HttpResponse('User creado satifactoriamente')
+                        login(request,user)
+                        return redirect('tasks')
                     
-                    except:
+                    except IntegrityError:
                            return render(request,'signup.html',{
                                 'form':UserCreationForm,
                                 'error': 'El usuario ya existe'
@@ -32,6 +34,9 @@ def singup(request):
                     'form':UserCreationForm,
                     'error': 'Pasword no coincide'
                 })
+        
+def tasks(request):
+       return render(request, 'tasks.html')    
                    
                         
 
